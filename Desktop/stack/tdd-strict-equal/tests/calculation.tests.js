78e81cb4cb838261a2e1a0adae2d176456dc13c6
Dave <dpenaloza2017@gmail.com>
2021-03-23 17:59:48 -0400

    Test

---
+++ Desktop/stack/tdd-strict-equal/tests/calculation.tests.js
@@ -0,0 +1,17 @@
+var describe = require('mocha').describe
+var it = require('mocha').it
+var strictEqual = require('../calculation')
+var expect = require('chai').expect
+
+describe('stricEqual', function () {
+  describe('strictEqual', () => {
+    it('return value of true if all the values are strictly equal to each other', () => {
+      // set up the test data
+      var testData = (5, 5)
+
+      // call function from outside file
+      strictEqual(testData)
+      // assert what should be true
+    }); expect().is.equal.to.strictEqual
+  })
+})
